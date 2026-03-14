251460f52f7b74c49c49a0d4b02a0d079ba977d2
Chris Johnstone <csjohnst@gmail.com>
2026-03-14 17:59:00 +0800

    feat: project scaffolding with bl_info and capture server package

---
+++ blender_mocap/__init__.py
@@ -0,0 +1,17 @@
+bl_info = {
+    "name": "Motion Capture",
+    "author": "Chris",
+    "version": (0, 1, 0),
+    "blender": (4, 5, 0),
+    "location": "View3D > Sidebar > Motion Capture",
+    "description": "Webcam motion capture with MediaPipe pose estimation",
+    "category": "Animation",
+}
+
+
+def register():
+    pass
+
+
+def unregister():
+    pass
